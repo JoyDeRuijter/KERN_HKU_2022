f5912476d148ecfc232d628afbe114acb6bf2c8b
Joy <joyderuijter@gmail.com>
2022-06-08 15:30:46 +0200

    Add player class

---
+++ Project1/Player.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Player
+{
+	private:
+		Vector2f position;
+		RectangleShape playerShape;
+		float playerSpeed = .3f;
+
+	public:
+		Player(float startX, float startY);
+		FloatRect getPosition();
+		RectangleShape getShape();
+		void moveLeft();
+		void moveRight();
+		void update();
+};
+
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Joy de Ruijter");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    //shape.setPosition(400.f, 400.f);
+    shape.move(5.f, 5.f);
 
     while (window.isOpen())
     {
